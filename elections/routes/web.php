6b859cf29a69b665a2f5a1718e9604a8bcb98336
stephanieTakam <stephanieTakam1@gmail.com>
2023-04-17 18:03:35 +0200

    creation de la migration pour region

--- elections/routes/web.php
+++ elections/routes/web.php
@@ -6,17 +6,17 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 Route::get('/token', function (Request $request) {
+
     $token = $request->session()->token();
 
     $token = csrf_token();
 
-    // ...
 });
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/region/create', [ RegionController::class, "create"]);
+Route::get('/region/create', [RegionController::class, "create"]);
 
-Route::post('/region_insert', [ RegionController::class, "insert"]);
+Route::post('/region_insert', [RegionController::class, "insert"]);
