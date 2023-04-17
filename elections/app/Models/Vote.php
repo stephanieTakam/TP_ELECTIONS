569feaa81852644ee7802bbf428acd4dff0c912d
stephanieTakam <stephanieTakam1@gmail.com>
2023-04-17 19:20:26 +0200

    application de la migration et creation du model Region

--- elections/app/Models/Vote.php
+++ elections/app/Models/Vote.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Bulletin extends Model
+class Vote extends Model
 {
     use HasFactory;
 }
